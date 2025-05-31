bc0d9109f250a6b753265028fb7ed1a08be67a6a
Ulrich <h.ulrich78160@gmail.com>
2025-05-31 15:50:40 +0200

    creation header et footer jsx et css et rajout logo

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Housing from './pages/Housing';
@@ -7,12 +10,16 @@ import Error404 from './pages/Error404';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/a-propos" element={<About />} />
         <Route path="/logement/:id" element={<Housing />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
+      
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ kasa/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
