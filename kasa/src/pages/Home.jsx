fd89e0fba9889f1c210d8a5c3999774100c2c36a
Ulrich <h.ulrich78160@gmail.com>
2025-05-31 16:11:38 +0200

    tentative d'ajustement page d'accueil

--- kasa/src/pages/Home.jsx
+++ kasa/src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import './Home.css';
 
 function Home() {
-  return <h2>Accueil</h2>;
+  return (
+    <main className="home">
+      <section className="banner container">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </section>
+
+      <section className="gallery container">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div key={index} className="card">
+            <p>Titre de la location</p>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 }
+
 export default Home;
