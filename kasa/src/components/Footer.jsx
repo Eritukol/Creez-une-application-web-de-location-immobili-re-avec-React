40bb863d3436a4b0200b18ebef00229a29f37331
Ulrich <h.ulrich78160@gmail.com>
2025-05-31 16:20:50 +0200

    changement couleur logo footer

--- kasa/src/components/Footer.jsx
+++ kasa/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import './Footer.css';
+import logo from '../assets/logo.png';
 
 function Footer() {
   return (
     <footer className="footer">
+      <img src={logo} alt="Kasa" className="footer__logo"/>
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
