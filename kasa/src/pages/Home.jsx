0ad2497460f98095789e35050017b74db124a569
Ulrich <h.ulrich78160@gmail.com>
2025-06-11 10:40:24 +0200

    recreation du dossier avec application du scss

---
+++ kasa/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main className='home'>
+      <h1>Bienvenue sur Kasa !</h1>
+    </main>
+  )
+}
+
+export default Home
--- kasa/src/router/Router.jsx
+++ kasa/src/router/Router.jsx
@@ -1,1 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+// Importation des composants de page
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Housing from '../pages/Housing'
+import Error404 from '../pages/Error404'
+
+// Importation des composants communs
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+// Centralisation des routes et composants communs
+function Router() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/a-propos' element={<About />} />
+        <Route path='/logement/:id' element={<Housing />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default Router
